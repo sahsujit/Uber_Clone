5e4469ec16fc836e60b695162d528db88c0ae3ab
sujit sah <sujit8877123sah@gmail.com>
2025-01-01 12:39:45 +0545

    adding register feature

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,21 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 const connectToDB = require("./db/db")
+const  userRoute = require("./routes/user.route")
 
 connectToDB();
 
-
-
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 
 app.get("/", (req, res)=>{
     res.send("Hello, World!");
 })
+app.use("/users", userRoute)
+
+
 
 module.exports = app;
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -8,5 +8,19 @@ module.exports.register= async(req, res, next)=>{
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    
+
+    const {fullname, email, password}= await req.body;
+
+    const hashPassword = await userModel.hashPassword(password);
+    const user = await userService.createUser({
+        firstname : fullname.firstname,
+        lastname : fullname.lastname,
+        email,
+        password: hashPassword
+    })
+
+    const token = user.generateAuthToken();
+    res.status(201).json({token, user})
+
+
 };
