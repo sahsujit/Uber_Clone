b87d93d407abbe6eccba37c1b5bf7bb4ff535e6f
sujit sah <sujit8877123sah@gmail.com>
2025-01-03 22:28:41 +0545

    logout controller created successfull

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -3,9 +3,14 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 module.exports.authUser = async(req, res, next) =>{
-    const token  = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token  = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     if(!token){
+        return res.status(401).json({message: "Unauthorized"})
+    }
+
+    const isBlaklisted = await userModel.findOne({token:token})
+    if(isBlaklisted){
         return res.status(401).json({message: "Unauthorized"})
     }
 
