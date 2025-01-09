9d9dfd46c5e0a842843c254440a261a8ae554a54
sujit sah <sujit8877123sah@gmail.com>
2025-01-09 20:33:04 +0545

    setting up frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen relative overflow-hidden'>
+         <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+    </div>
+  )
+}
+
+export default Home
