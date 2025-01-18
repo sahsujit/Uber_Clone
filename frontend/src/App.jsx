9c589673e8e9add97c65279de34af5c3018ccc05
sujit sah <sujit8877123sah@gmail.com>
2025-01-18 21:06:02 +0545

    debug solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Home from './pages/Home'
 import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 
 
 const App = () => {
@@ -32,7 +33,11 @@ const App = () => {
           </UserProtectWrapper>
           } />
 
-          <Route path='captain-home' element={<CaptainHome/>}/>
+          <Route path='captain-home' element={
+            <CaptainProtectWrapper>
+              <CaptainHome/>
+            </CaptainProtectWrapper>
+          }/>
       </Routes>
     </div>
   )
