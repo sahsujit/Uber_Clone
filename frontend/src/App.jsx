10dc4e268be170e0d41610dd055eb016e9a249d7
sahsujit <sujit8877123sah@gmail.com>
2025-02-01 18:43:36 +0545

    riding page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import Riding from './pages/Riding'
 
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
          </UserProtectWrapper>
           } />
         <Route path='/user-login' element={<UserLogin/>} />
+        <Route path='/riding' element={<Riding />} />
         <Route path='/user-signup' element={<UserSignup/>} />
         <Route path='/captain-login' element={<CaptainLogin/>} />
         <Route path='/captain-signup' element={<CaptainSignup/>} />
@@ -35,7 +37,7 @@ const App = () => {
 
           <Route path='/captain-home' element={
             <CaptainProtectWrapper>
-              <Home/>
+              <CaptainHome/>
             </CaptainProtectWrapper>
           }/>
       </Routes>
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
 
 const CaptainHome = () => {
   return (
-   <div></div>
-  )
-}
+    <div className="h-screen">
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
+        <img
+          className="w-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <Link
+          to="/captain-home"
+          className=" h-10 w-10 bg-white flex items-center justify-center rounded-full"
+        >
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
 
-export default CaptainHome
+      <div className="h-3/5">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
+      </div>
+
+      <div className="h-2/5 p-6">
+        <CaptainDetails />
+      </div>
+    </div>
+  );
+};
+
+export default CaptainHome;
