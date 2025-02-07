8227e127e2742e90ec1bfc15f8ca9b6f855f47e4
sahsujit <sujit8877123sah@gmail.com>
2025-02-07 22:12:58 +0545

    finish ride page added

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,9 +1,24 @@
 import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import FinishRide from '../components/FinishRide'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const CaptainRiding = () => {
     const [ finishRidePanel, setFinishRidePanel ] = useState(false)
     const finishRidePanelRef = useRef(null)
+
+    useGSAP(function () {
+        if (finishRidePanel) {
+            gsap.to(finishRidePanelRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(finishRidePanelRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+    }, [ finishRidePanel ])
   return (
     <div className='h-screen relative flex flex-col justify-end'>
 
@@ -15,9 +30,9 @@ const CaptainRiding = () => {
             </div>
 
             <div className='h-1/5 p-6 flex items-center justify-between relative bg-yellow-400 pt-10'
-                // onClick={() => {
-                //     setFinishRidePanel(true)
-                // }}
+                onClick={() => {
+                    setFinishRidePanel(true)
+                }}
             >
                 <h5 className='p-1 text-center w-[90%] absolute top-0' onClick={() => {
 
@@ -26,9 +41,9 @@ const CaptainRiding = () => {
                 <button className=' bg-green-600 text-white font-semibold p-3 px-10 rounded-lg'>Complete Ride</button>
             </div>
             <div ref={finishRidePanelRef} className='fixed w-full z-[500] bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-                {/* <FinishRide
+                <FinishRide
                     ride={rideData}
-                    setFinishRidePanel={setFinishRidePanel} /> */}
+                    setFinishRidePanel={setFinishRidePanel} />
             </div>
 
             <div className='h-screen fixed w-screen top-0 z-[-1]'>
